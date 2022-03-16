0bcf19f536cd993b39c008eccde02441285f59f3
Nishit Jain <yonishitjain123456@gmail.com>
2022-03-16 18:28:05 +0530

    Create Question6.py

---
+++ Question6.py
@@ -0,0 +1,6 @@
+# 6. SwaP cASe
+# Write a logic to transform the case of every character in a given string to its opposite case.
+# Example:
+# Input: enCycLOpedIA Output: ENcYCloPEDia
+
+print(input().swapcase())
